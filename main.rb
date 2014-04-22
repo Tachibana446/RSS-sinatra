bf62c06995e44d25cba253516142a47d0c383ddf
Tachibana446 <stinger33z@gmail.com>
2014-04-22 22:51:38 +0900

    RSSの記事一覧を表示することに成功

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'rss'
 
 get '/' do
-	site = "http://rss.rssad.jp/rss/h/wired/feed.rdf"
+	site = "http://feeds.feedburner.com/hatena/b/hotentry"
 	@rss = RSS::Parser.parse(site)
 	erb :index
 end
