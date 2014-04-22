2ff28cd25d62bc3e6c9f01e49f3a558fdea1656f
Tachibana446 <stinger33z@gmail.com>
2014-04-22 23:07:44 +0900

    Aboutページを追加
    sinatra/reloaderをrequireするとアプリを再起動しなくても変更が反映されるらしい

--- main.rb
+++ main.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require 'rss'
 
 get '/' do
 	site = "http://feeds.feedburner.com/hatena/b/hotentry"
 	@rss = RSS::Parser.parse(site)
 	erb :index
+end
+
+get '/about' do
+	"About this site"
 end
 
